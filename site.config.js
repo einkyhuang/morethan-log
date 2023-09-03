665204b58850ad70828e68f23ef132cd7ba6d3a2
einkyhuang <86918863+einkyhuang@users.noreply.github.com>
2023-09-03 04:18:50 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Digital Mall",
+    description: "Your digital world",
   },
 
   // CONFIG configration (required)
